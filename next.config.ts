3a9149134ef0f0d623a934a971dec60cdbde54c8
innowavesolution <innowavesolutioninfo@gmail.com>
2026-08-21 01:51:11 +0600

    update blog

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.api.jevxo.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.jevxo.com',
+      },
+    ],
+  },
   /* config options here */
   // @ts-expect-error - Next.js config type is slightly restrictive but these options work
   eslint: {
